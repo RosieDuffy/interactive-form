e595b0f1e134f9db140a675cc15a4fc9ee912ad7
RosieDuffy <rose.j.duffy@gmail.com>
2023-08-30 15:28:57 +0100

    T-Shirt section updated

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,39 @@
-
-const inputName = document.getElementById('name');
+const inputName = document.getElementById("name");
 
 inputName.focus();
 
-const jobRole = document.getElementById('title');
-const otherJobRole = document.getElementById('other-job-role');
+const jobRole = document.getElementById("title");
+const otherJobRole = document.getElementById("other-job-role");
 
 otherJobRole.style.display = "none";
 
-jobRole.addEventListener('change', (e) => {
-    let targetValue = e.target.value;
-    if(targetValue === "other"){
+jobRole.addEventListener("change", (e) => {
+  let targetValue = e.target.value;
+  if (targetValue === "other") {
+    otherJobRole.style.display = "block";
+  } else {
+    otherJobRole.style.display = "none";
+  }
+});
 
-        otherJobRole.style.display = "block"
-    } else{
-        otherJobRole.style.display = "none";
+const design = document.getElementById("design");
+const color = document.getElementById("color");
+const colorOptions = color.children;
+color.disabled = true;
+
+design.addEventListener("change", (e) => {
+  color.disabled = false;
+
+  for (let i = 0; i < colorOptions.length; i++) {
+    const targetValue = e.target.value;
+    const itemAttribute = colorOptions[i].getAttribute("data-theme");
+
+    if (targetValue === itemAttribute) {
+      colorOptions[i].hidden = false;
+      colorOptions[i].selected = true;
+    } else {
+      colorOptions[i].hidden = true;
+      colorOptions[i].selected = false;
     }
-    
-
-})
-
-
+  }
+});
