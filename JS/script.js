f3eff48581ba0a28eb580641060e89e1f530ad7f
RosieDuffy <rose.j.duffy@gmail.com>
2023-08-31 16:28:53 +0100

    Project complete

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,10 @@
+// Name Section - adding focus to 'Name' field //
+
 const inputName = document.getElementById("name");
 
 inputName.focus();
+
+// Job Role Section - displaying 'Other Job Role' field only when it is selected from the dropdown menu //
 
 const jobRole = document.getElementById("title");
 const otherJobRole = document.getElementById("other-job-role");
@@ -15,6 +19,8 @@ jobRole.addEventListener("change", (e) => {
     otherJobRole.style.display = "none";
   }
 });
+
+// T-Shirt Section - allowing only available designs to be selected to correspond with users selection //
 
 const design = document.getElementById("design");
 const color = document.getElementById("color");
@@ -38,6 +44,8 @@ design.addEventListener("change", (e) => {
   }
 });
 
+// Activities Section -keeps running total cost of activities selected by user //
+
 const activities = document.getElementById("activities");
 const totalElement = document.getElementById("activities-cost");
 let activitiesTotal = 0;
@@ -53,6 +61,8 @@ activities.addEventListener("change", (e) => {
 
   totalElement.innerHTML = `<p>Total: $${activitiesTotal}</p>`;
 });
+
+// Payment Info Section - displaying correct form information when payment type is selected by user //
 
 const payment = document.getElementById("payment");
 const creditCard = document.getElementById("credit-card");
@@ -80,3 +90,130 @@ payment.addEventListener("change", (e) => {
     bitcoin.style.display = "none";
   }
 });
+
+// Form Validation//
+
+const email = document.getElementById("email");
+const cardNumber = document.getElementById("cc-num");
+const zipCode = document.getElementById("zip");
+const cvv = document.getElementById("cvv");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  // Name Validation //
+
+  const nameValue = inputName.value;
+  const nameTest = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  const validName = nameTest.test(nameValue);
+
+  if (validName === false || nameValue === "") {
+    e.preventDefault();
+    inputName.parentElement.classList.add("not-valid");
+    inputName.parentElement.classList.remove("valid");
+    inputName.parentElement.lastElementChild.style.display = "inline";
+  } else {
+    inputName.parentElement.classList.add("valid");
+    inputName.parentElement.classList.remove("not-valid");
+    inputName.parentElement.lastElementChild.style.display = "none";
+  }
+
+  // Email Validation //
+
+  const emailValue = email.value;
+  const emailTest = /^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const validEmail = emailTest.test(emailValue);
+
+  if (validEmail === false || emailValue === "") {
+    e.preventDefault();
+    email.parentElement.classList.add("not-valid");
+    email.parentElement.classList.remove("valid");
+    email.parentElement.lastElementChild.style.display = "inline";
+  } else {
+    email.parentElement.classList.add("valid");
+    email.parentElement.classList.remove("not-valid");
+    email.parentElement.lastElementChild.style.display = "none";
+  }
+
+  // Activities Selected Validation //
+
+  if (activitiesTotal === 0) {
+    e.preventDefault();
+    activities.parentElement.classList.add("not-valid");
+    activities.parentElement.classList.remove("valid");
+    activities.parentElement.lastElementChild.style.display = "inline";
+  } else {
+    activities.parentElement.classList.add("valid");
+    activities.parentElement.classList.remove("not-valid");
+    activities.parentElement.lastElementChild.style.display = "none";
+  }
+
+  // Credit Card Validation //
+
+  if ((creditCard.style.display = "block")) {
+    // Credit Card Number Validation //
+
+    const cardNumberValue = cardNumber.value;
+    const cardNumberTest = /^\d{13,16}$/;
+    const validCardNumber = cardNumberTest.test(cardNumberValue);
+
+    if (validCardNumber === false || cardNumberValue === "") {
+      e.preventDefault();
+      cardNumber.parentElement.classList.add("not-valid");
+      cardNumber.parentElement.classList.remove("valid");
+      cardNumber.parentElement.lastElementChild.style.display = "inline";
+    } else {
+      cardNumber.parentElement.classList.add("valid");
+      cardNumber.parentElement.classList.remove("not-valid");
+      cardNumber.parentElement.lastElementChild.style.display = "none";
+    }
+
+    // Zip Code Validation //
+
+    const zipCodeValue = zipCode.value;
+    const zipCodeTest = /^\d{5}$/;
+    const validZipCode = zipCodeTest.test(zipCodeValue);
+
+    if (validZipCode === false || zipCodeValue === "") {
+      e.preventDefault();
+      zipCode.parentElement.classList.add("not-valid");
+      zipCode.parentElement.classList.remove("valid");
+      zipCode.parentElement.lastElementChild.style.display = "inline";
+    } else {
+      zipCode.parentElement.classList.add("valid");
+      zipCode.parentElement.classList.remove("not-valid");
+      zipCode.parentElement.lastElementChild.style.display = "none";
+    }
+
+    // CVV Validation //
+
+    const cvvValue = cvv.value;
+    const cvvTest = /^\d{3}$/;
+    const validCVV = cvvTest.test(cvvValue);
+
+    if (validCVV === false || cvvValue === "") {
+      e.preventDefault();
+      cvv.parentElement.classList.add("not-valid");
+      cvv.parentElement.classList.remove("valid");
+      cvv.parentElement.lastElementChild.style.display = "inline";
+    } else {
+      cvv.parentElement.classList.add("valid");
+      cvv.parentElement.classList.remove("not-valid");
+      cvv.parentElement.lastElementChild.style.display = "none";
+    }
+  } else {
+    e.preventDefault();
+  }
+});
+
+// Accessibilty - adding and removing focus on activities checkboxes //
+
+const activitiesChecked = document.querySelectorAll('input[type="checkbox"]');
+
+for (let i = 0; i < activitiesChecked.length; i++) {
+  activitiesChecked[i].addEventListener("focus", (e) => {
+    activitiesChecked[i].parentNode.classList.add("focus");
+  });
+  activitiesChecked[i].addEventListener("blur", (e) => {
+    activitiesChecked[i].parentNode.classList.remove("focus");
+  });
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # interactive-form
- Treehouse Project 3 - Interactive Form
+
+Treehouse Project 3 - Interactive Form
+
+Interactive form allowing users to sign up for a Full Stack Conference.
+User are able to enter their Name, Email, Job role and select certain T shirts based on their choices.
+
+Users can select conferences that they would like to attend and the website updates the running cost.
+
+Payment information can be submitted, users can pay with PayPal, Bitcoin or Credit Card.
+
+Full form validation is enabled, form will not be submitted if the correct information has not been entered.
